50b9c22ee5272d30ae53711e8f54b02a9972bf7b
Adrián Vázquez <adri.forzaferrari@gmail.com>
2022-11-15 22:11:11 +0100

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -3,6 +3,12 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class DataQualityOperator(BaseOperator):
+    """
+    :param redshift_conn_id: Redshift connection defined in Airflow
+    :param sql: SQL query which is going to be used as the quality check
+    :param table: Redshift table name where we are going run the sql query
+    :param expected_expression: Function used to test the output of the sql query
+    """
 
     ui_color = '#89DA59'
 
@@ -21,6 +27,12 @@ class DataQualityOperator(BaseOperator):
         self.expected_expression = expected_expression
 
     def execute(self, context):
+        """
+        Runs the query and validates the output using the expected_expression. 
+        If the result of running the expected expression against the output is true, it passes the test.
+        
+        :param context: Airflow context variables
+        """
         redshift_hook = PostgresHook(self.redshift_conn_id)
         records = redshift_hook.get_records(self.sql)
         
